0940417838c86a6ffcc056b57912caa21cea56ae
kadiyamPraveenKumar <141929065+kadiyamPraveenKumar@users.noreply.github.com>
2023-10-31 10:46:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+var clickSound1=new Audio()
+clickSound1.src="./type.mp3"
+function clickAudio1()
+{
+    clickSound1.play()
+}
+var clickSound2=new Audio()
+clickSound2.src="./type.mp3"
+function clickAudio2()
+{
+    clickSound2.play()
+}
+var clickSound3=new Audio()
+clickSound3.src="./type.mp3"
+function clickAudio3()
+{
+    clickSound3.play()
+}
+var clickSound4=new Audio()
+clickSound4.src="./type.mp3"
+function clickAudio4()
+{
+    clickSound4.play()
+}
+var clickSound5=new Audio()
+clickSound5.src="./type.mp3"
+function clickAudio5()
+{
+    clickSound5.play()
+}
+var clickSound6=new Audio()
+clickSound6.src="./type.mp3"
+function clickAudio6()
+{
+    clickSound6.play()
+}
+var clickSound7=new Audio()
+clickSound7.src="./type.mp3"
+function clickAudio7()
+{
+    clickSound7.play()
+}
+var clickSound8=new Audio()
+clickSound8.src="./type.mp3"
+function clickAudio8()
+{
+    clickSound8.play()
+}
+var clickSound9=new Audio()
+clickSound9.src="./type.mp3"
+function clickAudio9()
+{
+    clickSound9.play()
+}
+var clickSound10=new Audio()
+clickSound10.src="./type.mp3"
+function clickAudio10()
+{
+    clickSound10.play()
+}
+var clickSound=new Audio()
+clickSound.src="./type.mp3"
+function clickAudio()
+{
+    clickSound.play()
+}
+
